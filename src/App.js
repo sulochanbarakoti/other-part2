f4d52ca091a31eb38602d9fa69bef9a84a2e51ed
sulochan barakoti <sulochanbarakoti09@gmail.com>
2022-03-19 23:12:38 +0200

    upto 2.6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import './App.css';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
-const App = () => {
+const App = (props) => {
 
-  const [persons, setPersons] = setState([
-    {name: 'Arto Hellas'}
-  ])
-
+  const [persons, setPersons] = useState(props.notes)
   const [newName, setNewName] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  console.log(persons.length)
+  const addNote = (event) => {
+    // debugger
+    event.preventDefault()
+    // console.log("clicked",event.target.value)
+    const nameObject = {
+      name: newName,
+      date: new Date().toDateString(),
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
+    }
+    // console.log(persons)
+    setPersons(persons.concat(nameObject))
+    // console.log(persons)
+    setNewName('')
+
+  }
+
+  const handleNoteChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  // const showPerson = () => {
+  //   const showTruePerson = persons.filter(item => item.important === true)
+  //   setShowAll(showTruePerson)
+  // }
+
+  const nameToShow = showAll ? persons : persons.filter(item => item.important === true)
 
   return (
-    <div className="App">
-
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNote}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {nameToShow.map(item => 
+          <li key={item.id}>{item.name}</li>
+          )}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const notes = [
+  {
+    id: 1,
+    name: 'Sulochan',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    name: 'Avishek',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    name: 'Aanchal',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
 ReactDOM.render(
-    <App />,
+    <App notes={notes}/>,
   document.getElementById('root')
 );
 
