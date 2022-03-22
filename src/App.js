0928c063aaa0c1400ec17ef7c0697719743f3ba1
sulochan barakoti <sulochanbarakoti09@gmail.com>
2022-03-22 14:37:58 +0200

    upto 2.9

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 
 const App = (props) => {
 
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const [name, setName] = useState([])
+  const [search, setSearch] = useState('')
 
 
   // add button click event.
   const addNote = (event) => {
-    // debugger
-    event.preventDefault()
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-      date: new Date().toDateString(),
-      important: Math.random() < 0.5,
-      id: persons.length + 1,
-    }
-
-    // checking name in object and alert if name already exist
-    const newN = persons.filter(e=>e.name === newName)
-      if(newN.length==0){
-        setPersons(persons.concat(nameObject))
-      }
-      else{
-        window.alert(newName + ' is already in phonebook.')
-      }
+    event.preventDefault()      
+      if (newName !== '' && newNumber !== '') {
+        const nameObject = {
+          name: newName,
+          number: newNumber,
+          // date: new Date().toDateString(),
+          // important: Math.random() < 0.5,
+          id: persons.length + 1,
+        }
     
-    setNewName('')
-    setNewNumber('')
+        // checking name in object and alert if name already exist
+        const newN = persons.filter(e=>e.name === newName)
+          if(newN.length === 0){
+            setPersons(persons.concat(nameObject))
+          }
+          else{
+            window.alert(newName + ' is already in phonebook.')
+          }
+      } else {
+        window.alert('Please fill the form.')
+      }
+      setNewName('')
+      setNewNumber('')
+      setSearch('')
+    
   }
 
   // change event in textbox
@@ -44,12 +52,22 @@ const App = (props) => {
     setNewNumber(event.target.value)
   }
 
-  const nameToShow = showAll ? persons : persons.filter(item => item.important === true)
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+    console.log(event.target.value)
+  }
+  
+  // filter search
+  const nameToShow = (search === '') ? persons : persons.filter(item => item.name.match(search))
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Phonebook</h2><br></br>
+      <div>
+          filter shown with: <input value={search} onChange={handleSearchChange}/>
+      </div><br></br>
       <form onSubmit={addNote}>
+        <h1>add a new</h1>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
